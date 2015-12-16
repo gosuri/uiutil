8e15bb6ddd3f12208e152e1d4d461da9492d91f6
Greg Osuri <gosuri@gmail.com>
2015-12-15 16:35:06 -0800

    first take

---
+++ README.md
@@ -0,0 +1,9 @@
+# uiutil [![GoDoc](https://godoc.org/github.com/gosuri/uiutil?status.svg)](https://godoc.org/github.com/gosuri/uiutil) [![Build Status](https://travis-ci.org/gosuri/uiutil.svg?branch=master)](https://travis-ci.org/gosuri/uiutil)
+uiutil is a go library that provides a set of helpers to improve readbility in terminal applications.
+
+
+## Installation
+
+```
+$ go get github.com/gosuri/uiutil/...
+```
---
+++ printer.go
@@ -0,0 +1,61 @@
+package ui
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+// Out is the default out
+var Out = os.Stdout
+
+// Component in the interface that UI components need to implement
+type Component interface {
+	Bytes() []byte
+}
+
+// Printer represents a buffered container for components that can be flushed
+type Printer struct {
+	out   io.Writer
+	comps []Component
+}
+
+// NewPrinter returns a pointer to a new printer object
+func NewPrinter(out io.Writer) *Printer {
+	if out == nil {
+		out = Out
+	}
+	return &Printer{out: out}
+}
+
+// Add adds the components to the printer
+func (p *Printer) Add(c Component) *Printer {
+	p.comps = append(p.comps, c)
+	return p
+}
+
+// AddTitle Adds a Title to the printer
+func (p *Printer) AddTitle(title string) *Printer {
+	return p.Add(&Title{text: title})
+}
+
+// Bytes returns the formmated string of the output
+func (p *Printer) Bytes() []byte {
+	var buf bytes.Buffer
+	for _, c := range p.comps {
+		buf.Write(c.Bytes())
+		buf.Write([]byte{'\n'})
+	}
+	return buf.Bytes()
+}
+
+// Flush prints the output to the writer and clears the buffer
+func (p *Printer) Flush() error {
+	_, err := fmt.Fprintln(p.out, p)
+	if err != nil {
+		return err
+	}
+	p.comps = nil
+	return nil
+}
